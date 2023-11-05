b16d36eb76b4dd953275b4a861ad617c00753177
project-dy <kero122899@gmail.com>
2023-11-05 19:39:13 +0900

    Important settings

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 const express = require('express');
+const fs = require('fs');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => res.send(`
+/*app.get('/', (req, res) => res.send(`
 <html>
 <head>
 <title>My first express app</title>
@@ -11,7 +12,30 @@ app.get('/', (req, res) => res.send(`
 <h1>My first express app</h1>
 <p>Welcome to my first express app</p>
 </body>
-</html>`));
+</html>`));*/
+
+const path = require('path');
+const publicPath = path.resolve(__dirname, '../public').toString();
+
+// app.get('/', (req, res) => res.sendFile(`${publicPath}/index.html`));
+
+function getRoutePath(publicPath) {
+  fs.readdirSync(publicPath).forEach(file => {
+    try {
+      fs.readdirSync(path.resolve(publicPath, file));
+      getRoutePath(path.resolve(publicPath, file));
+    } catch {
+      const routePath = path.resolve(publicPath, file).toString().split('public')[1];
+      console.log(`routePath: ${routePath}`);
+      app.get(routePath, (req, res)=>{res.sendFile(`${path.resolve(publicPath, file).toString()}`)})
+      if (file === 'index.html') {
+        app.get(path.resolve(publicPath, file).toString().split('public')[1].split('index.html')[0], (req, res) => res.sendFile(`${path.resolve(publicPath, file).toString()}`));
+      }
+    }
+  });
+}
+
+getRoutePath(publicPath);
 
 app.listen(port,()=>{
   console.log(`Example app listening at http://localhost:${port}.`);
