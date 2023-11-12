4188cfc861cd5064821b2ae9ded6b61b4c03e15a
project-dy <kero122899@gmail.com>
2023-11-12 17:49:25 +0900

    Remove unnecessary import statement.

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const fs = require('fs');
 const bodyParser = require('body-parser');
-const { send } = require('process');
 const app = express();
 const port = 3000;
 
