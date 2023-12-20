7083dbe29aeb91287560c0221b581a903165bc75
project-dy <kero122899@gmail.com>
2023-12-20 21:20:56 +0900

    Add CSS styles and JavaScript functions for drawing and manipulating Tetris blocks.

--- public/game/web/js/app.js
+++ public/game/web/js/app.js
@@ -59,13 +59,222 @@ function drawInit() {
   // 20X10의 테트리스 칸을 그린다 이때 img태그를 사용한다. id에 0-0부터 19-9까지의 id를 부여한다.
   const tetris = document.getElementById("row");
   for (let i = 0; i < 20; i++) {
+    const row = document.createElement("div");
+    row.id = `row-${i}`; // row에 id를 부여한다.
+    row.className = "roww";
     for (let ii = 0; ii < 10; ii++) {
       let img = document.createElement("img");
-      img.src = `imgs/${ii+1}.png`;
       img.id = `${i}-${ii}`;
-      tetris.appendChild(img);
+      if (i == 19) { // CSS 확인용
+        // img.src = `imgs/${ii+1}.png`;
+      }
+      row.appendChild(img);
     }
-    tetris.appendChild(document.createElement("br"));
+    tetris.appendChild(row);
+    //tetris.appendChild(document.createElement("br"));
   }
 }
 drawInit();
+
+function drawBrick(block, x, y) {
+  // 블록을 그린다.
+  /*for (let i = 0; i < block.length; i++) {
+    for (let ii = 0; ii < block[0].length; ii++) {
+      if (block[i][ii] == 1) {
+        document.getElementById(`${y + i}-${x + ii}`).src = "imgs/1.png";
+      }
+    }
+  }*/
+  const tetris = document.getElementById("row");
+  const row = tetris.childNodes[y];
+  const img = row.childNodes[x];
+  if (block == 0) {
+    // img.removeAttribute("src");
+    img.src = "data:image/gif;base64,R0lGODlhAQABAAD/ACwAAAAAAQABAAACADs=";
+    return;
+  }
+  img.src = `imgs/${block}.png`;
+}
+
+window.drawBrick = drawBrick;
+
+/*block = [
+  [ // 사각형
+    [1, 1],
+    [1, 1],
+  ],
+  [ // 막대기
+    [1, 1, 1, 1],
+  ],
+  [ // ㄹ
+    [1, 1, 0],
+    [0, 1, 1],
+  ],
+  [ // 역 ㄹ
+    [0, 1, 1],
+    [1, 1, 0],
+  ],
+  [ // ㅗ
+    [0, 1, 0],
+    [1, 1, 1],
+  ],
+  [ // 역 ㄱ
+    [1, 1, 1],
+    [1, 0, 0],
+  ],
+  [ // ㄱ
+    [1, 1, 1],
+    [0, 0, 1],
+  ],
+];*/
+
+const block = [
+  [ // 사각형
+    [
+      [1, 1],
+      [1, 1],
+    ],
+    [
+      [1, 1],
+      [1, 1],
+    ],
+    [
+      [1, 1],
+      [1, 1],
+    ],
+    [
+      [1, 1],
+      [1, 1],
+    ]
+  ],
+  [ // 막대기
+    [
+      [1, 1, 1, 1],
+    ],
+    [
+      [1],
+      [1],
+      [1],
+      [1],
+    ],
+    [
+      [1, 1, 1, 1],
+    ],
+    [
+      [1],
+      [1],
+      [1],
+      [1],
+    ]
+  ],
+  [ // ㄹ
+    [
+      [1, 1, 0],
+      [0, 1, 1],
+    ],
+    [
+      [0, 1],
+      [1, 1],
+      [1, 0],
+    ],
+    [
+      [1, 1, 0],
+      [0, 1, 1],
+    ],
+    [
+      [0, 1],
+      [1, 1],
+      [1, 0],
+    ]
+  ],
+  [ // 역 ㄹ
+    [
+      [0, 1, 1],
+      [1, 1, 0],
+    ],
+    [
+      [1, 0],
+      [1, 1],
+      [0, 1],
+    ],
+    [
+      [0, 1, 1],
+      [1, 1, 0],
+    ],
+    [
+      [1, 0],
+      [1, 1],
+      [0, 1],
+    ]
+  ],
+  [ // ㅗ
+    [
+      [0, 1, 0],
+      [1, 1, 1],
+    ], 
+    [
+      [1, 0],
+      [1, 1],
+      [1, 0],
+    ],
+    [
+      [1, 1, 1],
+      [0, 1, 0],
+    ],
+    [
+      [0, 1],
+      [1, 1],
+      [1, 0],
+    ]
+  ],
+  [ // 역 ㄱ
+    [
+      [1, 1, 1],
+      [1, 0, 0],
+    ],
+    [
+      [1, 0],
+      [1, 0],
+      [1, 1],
+    ],
+    [
+      [0, 0, 1],
+      [1, 1, 1],
+    ],
+    [
+      [1, 1],
+      [0, 1],
+      [0, 1],
+    ],
+  ],
+  [ // ㄱ
+    [
+      [1, 1, 1],
+      [0, 0, 1],
+    ],
+    [
+      [0, 1],
+      [0, 1],
+      [1, 1],
+    ],
+    [
+      [1, 0, 0],
+      [1, 1, 1],
+    ],
+    [
+      [1, 1],
+      [1, 0],
+      [1, 0],
+    ]
+  ]
+];
+
+let blockData;
+
+blockData.ㅁ = block[0];
+blockData.ㅣ = block[1];
+blockData.ㄹ = block[2];
+blockData.ㄹㄹ = block[3];
+blockData.ㅗ = block[4];
+blockData.ㄱ = block[5];
+blockData.ㄱㄱ = block[6];
